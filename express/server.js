f072921930816c15c72780fcf0c98c0513449829
daitr92 <dai.tr@geekup.vn>
2019-06-16 10:47:52 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,11 +1,15 @@
 'use strict';
 const express = require('express');
+const Sentry = require('@sentry/node');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+Sentry.init({ dsn: 'https://bfbd168f5453439c92804fea15f2778e@sentry.io/1480669' });
+
 const router = express.Router();
 router.get('/', (req, res) => {
+  Sentry.captureMessage('Thằng Thành vừa post video @@');
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
